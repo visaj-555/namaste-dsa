b59de4bfb22fc02e09e0d3d39e1cf244a34add28
visaj-555 <visajpanchal777@gmail.com>
2026-02-03 22:55:47 +0530

    03-02-2026

---
+++ string/4-max-freq-sum.js
@@ -0,0 +1,32 @@
+var maxFreqSum = function (s) {
+    let map = {}
+    for (i = 0; i < s.length; i++) {
+        if (!map[s[i]]) {
+            map[s[i]] = 1
+        } else {
+            ++map[s[i]]
+        }
+    }
+
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let maxVowels = 0
+    let maxConsonant = 0
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            if (map[s[i]] > maxVowels) {
+                maxVowels = map[s[i]]
+            }
+        }
+        else {
+            if (map[s[i]] > maxConsonant) {
+                maxConsonant = map[s[i]]
+            }
+        }
+    }
+    return maxConsonant + maxVowels
+};        
+
+
+
+console.log(maxFreqSum("aaaaeeeiioouu")); 
+console.log(maxFreqSum("leetcode"));
