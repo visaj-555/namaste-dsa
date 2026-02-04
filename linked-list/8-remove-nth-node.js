58feff0d15cbdcf62002e9938cd9d8de48cad45a
visaj-cilans <visaj.cilans@gmail.com>
2026-02-04 22:25:22 +0530

    04-02-2026

--- linked-list/8-remove-nth-node.js
+++ linked-list/8-remove-nth-node.js
@@ -1,5 +1,7 @@
 //  19. Remove Nth Node From End of List
 
+
+// Approach 1 : Two Pass 
 var removeNthFromEnd = function(head, n) {
     let sentinel = new ListNode(0, head);
     let length = 0;
@@ -13,5 +15,32 @@ var removeNthFromEnd = function(head, n) {
         prev = prev.next;
     }
     prev.next = prev.next.next;
+    return sentinel.next;
+};
+
+
+// One Pass : 
+
+var removeNthFromEnd = function(head, n) {
+    let sentinel = new ListNode(0);
+    sentinel.next = head;
+
+    let first = sentinel;
+    let second = sentinel;
+
+    // move first ahead by n + 1 steps
+    for (let i = 0; i <= n; i++) {
+        first = first.next;
+    }
+
+    // move both pointers until first hits null
+    while (first !== null) {
+        first = first.next;
+        second = second.next;
+    }
+
+    // remove the nth node from the end
+    second.next = second.next.next;
+
     return sentinel.next;
 };
---
+++ string/5-balanced-spring-split.js
@@ -0,0 +1,13 @@
+var balancedStringSplit = function(s) {
+  let count = 0;
+  let balance = 0;
+
+  for (let char of s) {
+    if (char === 'L') balance++;
+    else balance--;
+
+    if (balance === 0) count++;
+  }
+
+  return count;
+};
