3adff5a18a855097cee8e862638c14320b8cd90c
visaj-cilans <visaj.cilans@gmail.com>
2026-02-05 09:07:11 +0530

    Added Easy Solution of Add 2 Numbers

--- linked-list/11-add-2-numbers.js
+++ linked-list/11-add-2-numbers.js
@@ -16,4 +16,33 @@ var addTwoNumbers = function(l1, l2) {
 
     return dummy.next;
 };
-    
+
+ //// Easy Solution : 
+
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+
+    while (l1 || l2 || carry) {
+        let sum = carry;
+
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        let digit = sum % 10;     // last digit to display         
+        carry = Math.floor(sum / 10);      // carry for next sum
+
+        current.next = new ListNode(digit);
+        current = current.next;
+    }
+
+    return dummy.next;
+};
