ad2aeee0fbf377404113c7ee63e5f46230897d6d
visaj-555 <visajpanchal777@gmail.com>
2026-02-15 21:06:05 +0530

    15-02-2026

---
+++ stack/8-daily-temparature.js
@@ -0,0 +1,25 @@
+// 739. Daily Temperatures
+
+
+var dailyTemperatures = function(arr) {
+    let stack = [];
+    let n = arr.length;
+    let ans = Array(n).fill(0);
+
+    stack.push(n-1);
+    for(let i = n-2; i >= 0; i--){
+        while(stack.length) {
+            let top = stack[stack.length-1];
+            if(arr[i] >= arr[top]) {
+                stack.pop();
+            } else {
+                ans[i] = top - i;
+                break;
+            }
+        }
+        stack.push(i);
+    }
+    return ans;
+};
+
+console.log(dailyTemperatures)
