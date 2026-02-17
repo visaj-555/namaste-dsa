95ccd51de0934cd0621e1aee089dde473b27b79d
visaj-cilans <visaj.cilans@gmail.com>
2026-02-17 22:26:36 +0530

    17-02-2025

--- stack/5-outermost-parenthesis.js
+++ stack/5-outermost-parenthesis.js
@@ -1,5 +1,30 @@
 //  1021. Remove Outermost Parentheses
 
+// Using Stack
+
+var removeOuterParentheses = function (s) {
+  let stack = [];
+  let ans = "";
+
+  for (let char of s) {
+    if (char === "(") {
+      if (stack.length > 0) {
+        ans += char; // not outer
+      }
+      stack.push(char);
+    } else {
+      stack.pop();
+      if (stack.length > 0) {
+        ans += char; // not outer
+      }
+    }
+  }
+
+  return ans;
+};
+
+// Without Using Stack
+
 function removeOuterParentheses(s) {
   let result = "";
   let balance = 0;
@@ -21,6 +46,17 @@ function removeOuterParentheses(s) {
   return result;
 }
 
-console.log(removeOuterParentheses("(()())(())"))
-console.log(removeOuterParentheses("(()())(())(()(()))"))
-console.log(removeOuterParentheses("()()"))
+console.log(removeOuterParentheses("(()())(())"));
+console.log(removeOuterParentheses("(()())(())(()(()))"));
+console.log(removeOuterParentheses("()()"));
+
+
+
+// Dry Run :  
+
+// (   → outer → skip
+// (   → inner → add
+// )   → inner → add
+// (   → inner → add
+// )   → inner → add
+// )   → outer → skip
