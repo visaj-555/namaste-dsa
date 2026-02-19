76900918df80fe9cce1a3e1cc8134478d39c5bab
visaj-555 <visajpanchal777@gmail.com>
2026-02-19 09:04:20 +0530

    19-02-2026

--- stack/7-next-greater-element.js
+++ stack/7-next-greater-element.js
@@ -1,37 +1,56 @@
 // 496. Next Greater Element I
 
-var nextGreaterElement = function(nums1, nums2) {
-    let stack = [];
-    let map = {};
+// Easy
 
-    // Build next greater map
-    for (let i = nums2.length - 1; i >= 0; i--) {
+function nextGreater(arr) {
+  let n = arr.length;
+  let ans = new Array(n).fill(-1);
+  let stack = [];
 
-        // Remove all smaller or equal elements
-        while (stack.length > 0 && stack[stack.length - 1] <= nums2[i]) {
-            stack.pop();
-        }
-
-        // If stack is empty, no greater element
-        if (stack.length === 0) {
-            map[nums2[i]] = -1;
-        } else {
-            map[nums2[i]] = stack[stack.length - 1];
-        }
-
-        stack.push(nums2[i]);
+  for (let i = 0; i < n; i++) {
+    while (stack.length > 0 && arr[i] > arr[stack[stack.length - 1]]) {
+      let prev = stack.pop();
+      ans[prev] = arr[i];
     }
 
-    // Build result array
-    let result = [];
-    for (let i = 0; i < nums1.length; i++) {
-        result.push(map[nums1[i]]);
+    stack.push(i);
+  }
+
+  return ans;
+}
+
+
+// Optimal with Map 
+
+var nextGreaterElement = function (nums1, nums2) {
+  let stack = [];
+  let map = {};
+
+  // Build next greater map
+  for (let i = nums2.length - 1; i >= 0; i--) {
+    // Remove all smaller or equal elements
+    while (stack.length > 0 && stack[stack.length - 1] <= nums2[i]) {
+      stack.pop();
     }
 
-    return result;
+    // If stack is empty, no greater element
+    if (stack.length === 0) {
+      map[nums2[i]] = -1;
+    } else {
+      map[nums2[i]] = stack[stack.length - 1];
+    }
+
+    stack.push(nums2[i]);
+  }
+
+  // Build result array
+  let result = [];
+  for (let i = 0; i < nums1.length; i++) {
+    result.push(map[nums1[i]]);
+  }
+
+  return result;
 };
 
-
-
-console.log(nextGreaterElement([4,1,2], [1,3,4,2]));
-console.log(nextGreaterElement([2,4], [1,2,3,4]));
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4]));
--- stack/8-daily-temparature.js
+++ stack/8-daily-temparature.js
@@ -1,27 +1,72 @@
 // 739. Daily Temperatures
 
+// Brute Force
 
-var dailyTemperatures = function(arr) {
-    let stack = [];
-    let n = arr.length;
-    let ans = Array(n).fill(0);
+function nextGreater(arr) {
+  let n = arr.length;
+  let ans = new Array(n).fill(-1);
 
-    stack.push(n-1);
-    for(let i = n-2; i >= 0; i--){
-        while(stack.length) {
-            let top = stack[stack.length-1];
-            if(arr[i] >= arr[top]) {
-                stack.pop();
-            } else {
-                ans[i] = top - i;
-                break;
-            }
-        }
-        stack.push(i);
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] > arr[i]) {
+        ans[i] = arr[j];
+        break;
+      }
     }
-    return ans;
+  }
+
+  return ans;
+}
+
+// Easy Optimal
+
+function dailyTemperatures(arr) {
+  let n = arr.length;
+  let ans = new Array(n).fill(0);
+  let stack = []; // store indices
+
+  for (let i = 0; i < n; i++) {
+
+    // current day is warmer
+    while (
+      stack.length > 0 &&
+      arr[i] > arr[stack[stack.length - 1]]
+    ) {
+      let prev = stack.pop();
+      ans[prev] = i - prev;
+    }
+
+    stack.push(i);
+  }
+
+  return ans;
+}
+
+
+
+// Optimal
+
+var dailyTemperatures = function (arr) {
+  let stack = [];
+  let n = arr.length;
+  let ans = Array(n).fill(0);
+
+  stack.push(n - 1);
+  for (let i = n - 2; i >= 0; i--) {
+    while (stack.length) {
+      let top = stack[stack.length - 1];
+      if (arr[i] >= arr[top]) {
+        stack.pop();
+      } else {
+        ans[i] = top - i;
+        break;
+      }
+    }
+    stack.push(i);
+  }
+  return ans;
 };
 
-console.log(dailyTemperatures([73,74,75,71,69,72,76,73]));
-console.log(dailyTemperatures([30,40,50,60]));
-console.log(dailyTemperatures([30,60,90]));
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
+console.log(dailyTemperatures([30, 40, 50, 60]));
+console.log(dailyTemperatures([30, 60, 90]));
