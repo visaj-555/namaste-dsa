0b09d9128044dc1574882023164b6f4c10f489d3
visaj-555 <visajpanchal777@gmail.com>
2026-02-26 08:58:54 +0530

    Solved : Search Rotated Array

--- binary-search/3-search-rotate-array.js
+++ binary-search/3-search-rotate-array.js
@@ -1,1 +1,36 @@
 // 33. Search in Rotated Sorted Array
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      // left half sorted
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      // right half sorted
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+console.log(search([4, 5, 6, 7, 0, 1, 2], 3));
+console.log(search([1], 0));
