76bfc6d66b96f18b4dd9b94e50af2ed982efded1
visaj-cilans <visaj.cilans@gmail.com>
2026-03-06 07:50:47 +0530

    Added Brute Force for better Clarity

--- two-pointers-&-sliding-window/2-two-sum-two.js
+++ two-pointers-&-sliding-window/2-two-sum-two.js
@@ -1,5 +1,27 @@
 //  167. Two Sum II - Input Array Is Sorted
 
+
+
+// Brute Force 
+
+var twoSum = function(numbers, target) {
+
+  for (let i = 0; i < numbers.length; i++) {
+
+    for (let j = i + 1; j < numbers.length; j++) {
+
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1]; // 1-indexed
+      }
+
+    }
+
+  }
+
+};
+
+// Optimal
+
 var twoSum = function (numbers, target) {
   let left = 0;
   let right = numbers.length - 1;
