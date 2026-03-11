369037fb8c98ab0a670f127b1e85da9f25111600
visaj-555 <visajpanchal777@gmail.com>
2026-03-11 22:32:15 +0530

    Added Brute Force and Improved the naming conention in Optimal Solution

--- pointers-and-sliding-window/8-trapping-rain-water.js
+++ pointers-and-sliding-window/8-trapping-rain-water.js
@@ -1,21 +1,56 @@
 // 42. Trapping Rain Water
 
-var trap = function (arr) {
-  let n = arr.length;
-  let maxL = [];
-  maxL[0] = arr[0];
-  for (let i = 1; i < n; i++) {
-    maxL[i] = Math.max(maxL[i - 1], arr[i]);
-  }
-  let maxR = [];
-  maxR[n - 1] = arr[n - 1];
-  for (let i = n - 2; i >= 0; i--) {
-    maxR[i] = Math.max(arr[i], maxR[i + 1]);
-  }
-  let ans = 0;
+// Brute Force  :
+
+var trap = function (height) {
+  let n = height.length;
+  let water = 0;
+
   for (let i = 0; i < n; i++) {
-    let waterTrapped = Math.min(maxL[i], maxR[i]) - arr[i];
-    ans += Math.max(waterTrapped, 0); // Avoid negative values
+    let leftMax = 0;
+    let rightMax = 0;
+
+    // find max on left
+    for (let j = 0; j <= i; j++) {
+      leftMax = Math.max(leftMax, height[j]);
+    }
+
+    // find max on right
+    for (let j = i; j < n; j++) {
+      rightMax = Math.max(rightMax, height[j]);
+    }
+
+    water += Math.min(leftMax, rightMax) - height[i];
   }
-  return ans;
+
+  return water;
+};
+
+// Optimal
+
+var trap = function (height) {
+  let n = height.length;
+
+  let leftMax = new Array(n);
+  let rightMax = new Array(n);
+
+  leftMax[0] = height[0];
+
+  for (let i = 1; i < n; i++) {
+    leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+  }
+
+  rightMax[n - 1] = height[n - 1];
+
+  for (let i = n - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+  }
+
+  let water = 0;
+
+  for (let i = 0; i < n; i++) {
+    water += Math.min(leftMax[i], rightMax[i]) - height[i];
+  }
+
+  return water;
 };
