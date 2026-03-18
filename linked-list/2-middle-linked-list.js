60e8669b1f03e43c0eb1a5f6c14860aac69428d7
visaj-cilans <visaj.cilans@gmail.com>
2026-03-18 08:26:12 +0530

    Added Brute force while doing revision of linked list

--- linked-list/2-middle-linked-list.js
+++ linked-list/2-middle-linked-list.js
@@ -1,5 +1,28 @@
 // 876. Middle Linked List
 
+// Brute Force
+
+var middleNode = function (head) {
+  let count = 0;
+  let curr = head;
+
+  // count nodes
+  while (curr) {
+    count++;
+    curr = curr.next;
+  }
+
+  // go to middle
+  let mid = Math.floor(count / 2);
+  curr = head;
+
+  for (let i = 0; i < mid; i++) {
+    curr = curr.next;
+  }
+
+  return curr;
+};
+
 var middleNode = function (head) {
   let slow = head;
   let fast = head;
