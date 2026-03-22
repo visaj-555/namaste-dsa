87c9f576c441a412b49a823f54e87e45a5fb584d
visaj-555 <visajpanchal777@gmail.com>
2026-03-22 23:27:24 +0530

    Revised Insertion Sort

--- sorting/3-insertion-sort.js
+++ sorting/3-insertion-sort.js
@@ -1,14 +1,18 @@
 function insertionSort(arr) {
   let n = arr.length;
+
   for (let i = 1; i < n; i++) {
     let curr = arr[i];
     let prev = i - 1;
-    while (arr[prev] > curr && prev >= 0) {
-      arr[prev + 1] = arr[prev];
+
+    while (prev >= 0 && arr[prev] > curr) {
+      arr[prev + 1] = arr[prev]; // shift
       prev--;
     }
-    arr[prev + 1] = curr;
+
+    arr[prev + 1] = curr; // insert
   }
+
   return arr;
 }
 
