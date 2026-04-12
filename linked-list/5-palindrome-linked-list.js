8c9a411b362a2e0fef6867c994fb34e806fe7668
visaj-cilans <visaj.cilans@gmail.com>
2026-04-12 09:16:53 +0530

    Revisited Palindrome Linkedlist

--- linked-list/5-palindrome-linked-list.js
+++ linked-list/5-palindrome-linked-list.js
@@ -1,29 +1,50 @@
- // 234. Palindrome Linked List
+// 234. Palindrome Linked List
 
-var isPalindrome = function(head) {
-    let slow = head, fast = head;
+// Brute Force :
 
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
+var isPalindrome = function (head) {
+  let arr = [];
+  let curr = head;
+  while (curr !== null) {
+    arr.push(curr.val);
+    curr = curr.next;
+  }
+  let left = 0,
+    right = arr.length - 1;
+  while (left < right) {
+    if (arr[left++] !== arr[right--]) return false;
+  }
+  return true;
+};
 
-    // Reverse second half
-    let prev = null;
-    while (slow) {
-        let temp = slow.next;
-        slow.next = prev;
-        prev = slow;
-        slow = temp;
-    }
+// Optimal :
 
-    // Compare two halves
-    let first = head, second = prev;
-    while (second) {
-        if (first.val !== second.val) return false;
-        first = first.next;
-        second = second.next;
-    }
+var isPalindrome = function (head) {
+  let slow = head,
+    fast = head;
 
-    return true;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Reverse second half
+  let prev = null;
+  while (slow) {
+    let temp = slow.next;
+    slow.next = prev;
+    prev = slow;
+    slow = temp;
+  }
+
+  // Compare two halves
+  let first = head,
+    second = prev;
+  while (second) {
+    if (first.val !== second.val) return false;
+    first = first.next;
+    second = second.next;
+  }
+
+  return true;
 };
