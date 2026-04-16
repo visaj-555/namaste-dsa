a4a85e41d962a663ac3cfa7748e7dbcbc83545fa
visaj-555 <visajpanchal777@gmail.com>
2026-04-16 15:25:59 +0530

    Added Optimal Solution for Intersection of Linked List

--- linked-list/6-intersection.js
+++ linked-list/6-intersection.js
@@ -1,16 +1,31 @@
 // 160. Intersection of Two Linked Lists
 
+var getIntersectionNode = function (headA, headB) {
+  // Brute Force :
 
-var getIntersectionNode = function(headA, headB) {
-    let set = new Set();
-    while (headB) {
-        set.add(headB);
-        headB = headB.next;
-    }
-    while (headA) {
-        if (set.has(headA)) return headA;
-        headA = headA.next;
-    }
-    return null;
+  let set = new Set();
+  
+  while (headB) {
+    set.add(headB);
+    headB = headB.next;
+  }
+  while (headA) {
+    if (set.has(headA)) return headA;
+    headA = headA.next;
+  }
+  return null;
 };
-    
+
+// Optimal :
+
+var getIntersectionNode = function (headA, headB) {
+  let a = headA;
+  let b = headB;
+
+  while (a !== b) {
+    a = a ? a.next : headB;
+    b = b ? b.next : headA;
+  }
+
+  return a;
+};
