8d1fc4d751a4b0a4610b70b5a8591eda4f4bbda4
visaj-cilans <visaj.cilans@gmail.com>
2026-04-26 20:28:41 +0530

    Added easy brute force for Balanced Strings

--- string/5-balanced-spring-split.js
+++ string/5-balanced-spring-split.js
@@ -1,11 +1,36 @@
 //  1221. Split a String in Balanced Strings
 
-var balancedStringSplit = function(s) {
+// Brute Force Solution
+
+var balancedStringSplit = function (s) {
+  let R = 0;
+  let L = 0;
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "R") {
+      ++R;
+    } else {
+      ++L;
+    }
+
+    if (L == R) {
+      count++;
+      R = 0;
+      L = 0;
+    }
+  }
+  return count;
+};
+
+// Optimal Solution
+
+var balancedStringSplit = function (s) {
   let count = 0;
   let balance = 0;
 
   for (let char of s) {
-    if (char === 'L') balance++;
+    if (char === "L") balance++;
     else balance--;
 
     if (balance === 0) count++;
