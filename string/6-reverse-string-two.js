ce1b6baeffb0a2133265a84963cd4a0130de2672
visaj-555 <visajpanchal777@gmail.com>
2026-04-27 21:44:07 +0530

    Added Brute Force for Reverse String - 2

--- string/6-reverse-string-two.js
+++ string/6-reverse-string-two.js
@@ -1,22 +1,40 @@
-var reverseStr = function(s, k) {
-    let arr = s.split("");
+// Brute Force :
 
-    for (let i = 0; i < arr.length; i += 2 * k) {
-        let left = i;
-        let right = Math.min(i + k - 1, arr.length - 1);
+var reverseStr = function (s, k) {
+  let result = "";
 
-        while (left < right) {
-            [arr[left], arr[right]] = [arr[right], arr[left]];
-            left++;
-            right--;
-        }
-    }
+  for (let i = 0; i < s.length; i += 2 * k) {
+    let first = s
+      .slice(i, i + k)
+      .split("")
+      .reverse()
+      .join("");
+    let second = s.slice(i + k, i + 2 * k);
 
-    return arr.join("");
+    result += first + second;
+  }
+
+  return result;
 };
 
+// Optimal :
 
-console.log(reverseStr("abcdefg", 2))
-console.log(reverseStr("abcd", 2))
+var reverseStr = function (s, k) {
+  let arr = s.split("");
 
+  for (let i = 0; i < arr.length; i += 2 * k) {
+    let left = i;
+    let right = Math.min(i + k - 1, arr.length - 1);
 
+    while (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+
+  return arr.join("");
+};
+
+console.log(reverseStr("abcdefg", 2));
+console.log(reverseStr("abcd", 2));
