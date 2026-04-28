b2813e72d94227b46e3cfe69411906a6a94ca871
visaj-555 <visajpanchal777@gmail.com>
2026-04-28 08:43:46 +0530

    Added Easy Solution for Largest Odd number

--- string/8-largest-odd-number.js
+++ string/8-largest-odd-number.js
@@ -1,18 +1,30 @@
 // 1903. Largest Odd Number in String
 
+// Using while loop and substring
 
-  var largestOddNumber = function(s) {
-      let n = s.length - 1;
-      while (n >= 0) {
-          if (Number(s[n]) % 2 == 1) {
-              return s.substring(0, n + 1);
-          }
-          --n;
-      }
-      return "";
-  };
-  
-  
-  console.log(largestOddNumber("52"));
-  console.log(largestOddNumber("4602")); 
-  console.log(largestOddNumber("35427"));
+var largestOddNumber = function (s) {
+  let n = s.length - 1;
+  while (n >= 0) {
+    if (Number(s[n]) % 2 == 1) {
+      return s.substring(0, n + 1);
+    }
+    --n;
+  }
+  return "";
+};
+
+// Using for loop and slice
+
+var largestOddNumber = function (num) {
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (Number(num[i]) % 2 !== 0) {
+      return num.slice(0, i + 1);
+    }
+  }
+
+  return "";
+};
+
+console.log(largestOddNumber("52"));
+console.log(largestOddNumber("4602"));
+console.log(largestOddNumber("35427"));
