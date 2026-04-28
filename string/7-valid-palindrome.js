10524ab270e1a49bf546b2940227b5c9fa6244f6
visaj-555 <visajpanchal777@gmail.com>
2026-04-28 07:47:23 +0530

    Added comments in palindrome for better code reading

--- string/7-valid-palindrome.js
+++ string/7-valid-palindrome.js
@@ -3,11 +3,11 @@
 // Extra Space
 
 function isPalindrome(str) {
-  str = str.toLowerCase(); 
+  str = str.toLowerCase();
   let filteredString = "";
-  let rev = ""; 
+  let rev = "";
 
-  for (let i = 0; i < str.length; i++) { 
+  for (let i = 0; i < str.length; i++) {
     if (str[i].match(/[a-z0-9]/i)) {
       filteredString += str[i];
       rev = str[i] + rev;
@@ -19,24 +19,29 @@ function isPalindrome(str) {
 
 // Two Pointers
 
- var isPalindrome = function(s) {
-    s = s.toLowerCase();
-    let i = 0;
-    let j = s.length - 1;
-    while (i < j) {
-        if (!s[i].match(/[a-z0-9]/i)) {
-            ++i;
-        }
-        else if (!s[j].match(/[a-z0-9]/i)) {
-            --j;
-        }
-        else if (s[i] === s[j]) {
-            ++i;
-            --j;
-        }
-        else {
-            return false;
-        }
+var isPalindrome = function (s) {
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    // skip non-alphanumeric from left
+    while (l < r && !/[a-z0-9]/i.test(s[l])) {
+      l++;
     }
-    return true;
-  };
+
+    // skip non-alphanumeric from right
+    while (l < r && !/[a-z0-9]/i.test(s[r])) {
+      r--;
+    }
+
+    // compare (case-insensitive)
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+      return false;
+    }
+
+    l++;
+    r--;
+  }
+
+  return true;
+};
