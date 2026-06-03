ce537ef9211d40a70df8f773a9db48d60c9b0a8f
visaj-555 <visajpanchal777@gmail.com>
2026-06-03 09:55:29 +0530

    Revision : Easy Solution

--- array/1-remove-duplicates.js
+++ array/1-remove-duplicates.js
@@ -1,16 +1,15 @@
-function removeDuplicates(nums) {
-  if (nums.length === 0) return 0;
-
+var removeDuplicates = function (nums) {
   let x = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > nums[x]) {
-      x = x + 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[x]) {
+      x++;
       nums[x] = nums[i];
     }
   }
 
   return x + 1;
-}
+};
 
 console.log(removeDuplicates([1, 1, 2]));
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
