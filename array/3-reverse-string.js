f9a99add0f346c63309b5d1ed5844a8280fa1e1b
visaj-555 <visajpanchal777@gmail.com>
2026-06-03 09:55:29 +0530

    Easy Solution : Reverse String

--- array/3-reverse-string.js
+++ array/3-reverse-string.js
@@ -1,29 +1,32 @@
 // 344. Reverse String
 
 var reverseString = function (s) {
-  let length = s.length;
-  let halfLength = Math.floor(length / 2);
-  for (let i = 0; i < halfLength; i++) {
-    let temp = s[i];
-    s[i] = s[length - 1 - i];
-    s[length - 1 - i] = temp;
+  let mid = Math.floor(s.length / 2);
+  let left = 0;
+  let right = s.length - 1;
+  let temp = 0;
+
+  for (let i = 0; i < mid; i++) {
+    temp = s[right];
+    s[right] = s[left];
+    s[left] = temp;
+    left++;
+    right--;
   }
 };
 
-// More Standard Solution 
+// More Standard Solution
 
-var reverseString = function(s) {
-    let left = 0;
-    let right = s.length - 1;
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
 
-    while (left < right) {
-        [s[left], s[right]] = [s[right], s[left]];
-        left++;
-        right--;
-    }
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
 };
-
-
 
 console.log(reverseString(["h", "e", "l", "l", "o"]));
 console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
