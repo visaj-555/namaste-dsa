062524e53459728ea8845911146e3c0b4b4b6f2b
visaj-555 <visajpanchal777@gmail.com>
2026-06-03 09:55:30 +0530

    Revision : 3 pointers approach

--- array/5-merge-sorted-array.js
+++ array/5-merge-sorted-array.js
@@ -43,7 +43,24 @@ var merge = function (nums1, m, nums2, n) {
   }
 };
 
+// Approach 4 : Three pointers
 
+var merge = function (nums1, m, nums2, n) {
+  let a = m - 1;
+  let b = n - 1;
+  let c = m + n - 1;
+
+  while (b >= 0) {
+    if (a >= 0 && nums1[a] > nums2[b]) {
+      nums1[c] = nums1[a];
+      a--;
+    } else {
+      nums1[c] = nums2[b];
+      b--;
+    }
+    c--;
+  }
+};
 
 console.log(((a) => (merge(a, 3, [2, 5, 6], 3), a))([1, 2, 3, 0, 0, 0]));
 console.log(((a) => (merge(a, 1, [], 0), a))([1]));
