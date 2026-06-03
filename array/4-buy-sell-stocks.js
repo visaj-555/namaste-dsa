a2519773d891bed75cd1c4c73769a3c2881be832
visaj-555 <visajpanchal777@gmail.com>
2026-06-03 09:55:29 +0530

    Simplified while revision

--- array/4-buy-sell-stocks.js
+++ array/4-buy-sell-stocks.js
@@ -1,6 +1,6 @@
 // 121. Best Time to Buy and Sell Stock
 
-function maxProfit(prices) {
+var maxProfit = function (prices) {
   let min_price = prices[0],
     profit = 0,
     max_profit = 0;
@@ -12,7 +12,7 @@ function maxProfit(prices) {
   }
 
   return max_profit;
-}
+};
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 console.log(maxProfit([7, 6, 4, 3, 1]));
